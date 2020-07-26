c0236398ebf6883bae233d8053c8d9b224c00636
Sallin <sallin.prod@gmail.com>
2020-07-26 14:04:51 +0300

    Updates

---
+++ precache-manifest.c0a43f3f48e586a9678efd22778a9d82.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7de72c591bf7a274c624a41fbf5cc2d6",
+    "url": "/%7Brepo-name%7D/index.html"
+  },
+  {
+    "revision": "5ad40a002b1c0fd6448c",
+    "url": "/%7Brepo-name%7D/static/css/main.32c4ae58.chunk.css"
+  },
+  {
+    "revision": "be19641d3fe78d35223f",
+    "url": "/%7Brepo-name%7D/static/js/2.ef8b4283.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/%7Brepo-name%7D/static/js/2.ef8b4283.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ad40a002b1c0fd6448c",
+    "url": "/%7Brepo-name%7D/static/js/main.34681b97.chunk.js"
+  },
+  {
+    "revision": "27017af5429ee0e73071",
+    "url": "/%7Brepo-name%7D/static/js/runtime-main.a110b937.js"
+  }
+]);
